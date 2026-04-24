dcec03d85b23a147742e3b05fcdb4ceecfc42827
cen <eltonjaycimafranca2006@gmail.com>
2026-04-24 20:12:35 +0800

    Updated portfolio design and fixes, and added home design

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,9 @@ const sidebar = document.getElementById("sidebar");
 const main = document.getElementById("homepage");
 const mainContent = document.querySelector("main");
 const homepage = document.getElementById("homepage");
+const viewBtn = document.querySelector(".view_button");
+const contactBtn = document.querySelector(".contact_button");
+const bar = document.getElementById("bar");
 
 bar.addEventListener("click", () => {
   sidebar.classList.toggle("active");
@@ -17,4 +20,18 @@ document.querySelectorAll(".card").forEach(card => {
       section.scrollIntoView({ behavior: "smooth" });
     }
   });
+});
+
+viewBtn.addEventListener("click", () => {
+  const projectsSection = document.getElementById("project");
+  if(projectsSection) {
+    projectsSection.scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+contactBtn.addEventListener("click", () => {
+  const contactSection = document.getElementById("contact");
+  if(contactSection) {
+    contactSection.scrollIntoView({ behavior: "smooth" });
+  }
 });
